a73579e89c66e034b6a0ee94b5bfb2d74a9f7a9b
silverviles <sudarakatharindu2001@gmail.com>
2025-12-14 19:12:12 +0530

    fix: restart python project

--- hardware-abstraction-layer/pkg/crypto/crypto.go
+++
@@ -1,8 +0,0 @@
-package crypto
-
-import "context"
-
-type Crypto interface {
-	Encrypt(ctx context.Context, data interface{}, saltSize int) ([]byte, error)
-	Decrypt(ctx context.Context, data []byte) (interface{}, error)
-}
--- hardware-abstraction-layer/pkg/keystore/keystore.go
+++
@@ -1,9 +0,0 @@
-package keystore
-
-import "context"
-
-type Keystore interface {
-	Get(ctx context.Context, key, version string) (interface{}, error)
-	Set(ctx context.Context, key string, value interface{}) error
-	Delete(ctx context.Context, key, version string) error
-}
